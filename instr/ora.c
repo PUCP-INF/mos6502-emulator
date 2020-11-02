19e03b839641fc189ec2333bdf8e9b572e705321
ribartra <superricardo5000@gmail.com>
2020-11-02 00:52:04 -0500

    ALGUNAS INSTRUCCIONES TERMINADAS + BANDERAS

--- instr/ora.c
+++ instr/ora.c
@@ -3,12 +3,42 @@
 //
 
 #include "ora.h"
+#include "cpu.h"
 
 void oraxind()
-{}
+{
+    uint8_t dm=get_arg(1)+cpu.x;
+    uint16_t high=mem.ram[0][dm+1];
+    uint16_t low=mem.ram[0][dm];
+    cpu.a |=mem.ram[high][low];
+
+    if(!cpu.a)
+        setsr(1);
+    else
+        unsetsr(1);
+
+    if(cpu.a & 0b10000000)
+        setsr(7);
+    else
+        unsetsr(7);
+
+}
 
 void orazpg()
-{}
+{
+    uint8_t offset = get_arg(1);
+    cpu.a |=mem.ram[0][offset];
+
+    if(!cpu.a)
+        setsr(1);
+    else
+        unsetsr(1);
+
+    if(cpu.a &0b10000000)
+        setsr(7);
+    else
+        unsetsr(7);
+}
 
 void oraimmd()
 {}
@@ -17,10 +47,41 @@ void oraabs()
 {}
 
 void oraindy()
-{}
+{
+    uint8_t dm = get_arg(1);
+    uint8_t high=mem.ram[0][dm+1];
+    uint8_t low=mem.ram[0][dm];
+    uint16_t offset =(high<<8)+low+cpu.y;
+    cpu.a |=mem.ram[offset>>8][offset & 0XFF];
+
+    if(!cpu.a)
+        setsr(1);
+    else
+        unsetsr(1);
+
+    if(cpu.a &0b10000000)
+        setsr(7);
+    else
+        unsetsr(7);
+
+}
 
 void orazpgx()
-{}
+{
+    uint8_t offset = get_arg(1)+cpu.x;
+    cpu.a |=mem.ram[0][offset];
+
+    if(!cpu.a)
+        setsr(1);
+    else
+        unsetsr(1);
+
+    if(cpu.a &0b10000000)
+        setsr(7);
+    else
+        unsetsr(7);
+
+}
 
 void oraabsy()
 {}
--- instr/rol.c
+++ instr/rol.c
@@ -3,9 +3,34 @@
 //
 
 #include "rol.h"
+#include "cpu.h"
 
 void rolzpg()
-{}
+{
+    uint8_t offset = get_arg(1);
+    uint8_t elem=mem.ram[0][offset];
+    if(elem & 0b10000000){
+        setsr(0);
+        elem<<=1;
+        elem|=0b00000001;
+    }
+    else{
+        unsetsr(0);
+        elem<<=1;
+    }
+    if(!elem)
+        setsr(1);
+    else
+        unsetsr(1);
+
+    if(elem>127)
+        setsr(6);
+    else
+        unsetsr(6);
+
+    mem.ram[0][offset]=elem;
+
+}
 
 void rola()
 {}
@@ -14,7 +39,27 @@ void rolabs()
 {}
 
 void rolzpgx()
-{}
+{
+    uint8_t offset = get_arg(1)+cpu.x;
+    uint8_t elem=mem.ram[0][offset];
+    if(elem & 0b10000000){
+        setsr(0);
+        elem<<=1;
+        elem|=0b00000001;
+    }
+    else{
+        unsetsr(0);
+        elem<<=1;
+    }
+
+    if(elem>127)
+        setsr(6);
+    else
+        unsetsr(6);
+
+
+    mem.ram[0][offset]=elem;
+}
 
 void rolabsx()
 {}
