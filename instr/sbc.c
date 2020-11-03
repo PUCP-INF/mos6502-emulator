1f72d2bfbfebc2882155b23d36788831058f92d1
joaquin aragon <theterx118@gmail.com>
2020-11-02 19:45:27 -0500

    falta agregar los cambios de bandera zeronegflags, no estaba seguro de si hacerlos o es algo del grupo e.e

--- instr/sbc.c
+++ instr/sbc.c
@@ -3,7 +3,7 @@
 //
 
 #include "sbc.h"
-
+#include "cpu.h"
 void sbcxind()
 {}
 
@@ -17,10 +17,38 @@ void sbczpgx()
 {}
 
 void sbcimm()
-{}
+{
+    uint8_t value_mem = get_arg(1);
+
+    cpu.a -= value_mem;
+    uint8_t value_a = cpu.a;
+
+    //modify C flag
+    if(value_a<0)unsetsr(0);
+    else setsr(0);
+    //modify N flag
+    if(value_a<0)setsr(7);
+    //modify z flag
+    if(value_a==0)setsr(1);
+}
 
 void sbcabsy()
-{}
+{
+    cpu.y = 1;
+    uint8_t high = get_arg(2) ,low= get_arg(1) ;
+
+    uint8_t value_mem = mem.ram[high][low];
+    cpu.a -= value_mem + cpu.y;
+    uint8_t value_a = cpu.a;
+
+    //modify C flag
+    if(value_a<0)unsetsr(0);
+    else setsr(0);
+    //modify N flag
+    if(value_a<0)setsr(7);
+    //modify z flag
+    if(value_a==0)setsr(1);
+}
 
 void sbcabs()
 {}
