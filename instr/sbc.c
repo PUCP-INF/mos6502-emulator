f3c8059cc01e9cb1fa9f39132bc8270f46162d40
Sebastian Davila Diaz <sebastian.davila@pucp.edu.pe>
2020-11-08 18:20:58 -0500

    Dos que faltaban

--- instr/sbc.c
+++ instr/sbc.c
@@ -6,7 +6,29 @@
 #include "cpu.h"
 
 void sbcxind()
-{}
+{
+    uint8_t dm=get_arg(1)+cpu.x;
+    uint8_t high=mem.ram[0][dm+1];
+    uint8_t low=mem.ram[0][dm];
+    cpu.a= cpu.a-mem.ram[high][low];
+    uint8_t value_a = cpu.a;
+
+    //modify C flag
+    if(value_a<0)unsetsr(0);
+    else setsr(0);
+    //modify N flag
+    if(value_a<0)setsr(7);
+    else unsetsr(7);
+    //modify z flag
+    if(value_a==0)setsr(1);
+    else unsetsr(1);
+    //modifi V flag
+    if(value_a > 127 || value_a < -127) {
+        setsr(6);
+    } else {
+        unsetsr(6);
+    }
+}
 
 void sbcindy()
 {}
@@ -24,7 +46,29 @@ void sbcabsy()
 {}
 
 void sbcabs()
-{}
+{
+    uint8_t low, high;
+    low=get_arg(1);
+    high=get_arg(2);
+    cpu.a=cpu.a-mem.ram[high][low];
+    uint8_t value_a = cpu.a;
+
+    //modify C flag
+    if(value_a<0)unsetsr(0);
+    else setsr(0);
+    //modify N flag
+    if(value_a<0)setsr(7);
+    else unsetsr(7);
+    //modify z flag
+    if(value_a==0)setsr(1);
+    else unsetsr(1);
+    //modifi V flag
+    if(value_a > 127 || value_a < -127) {
+        setsr(6);
+    } else {
+        unsetsr(6);
+    }
+}
 
 void sbcabsx()
 {
@@ -35,7 +79,7 @@ void sbcabsx()
     high= offset/256;
     low= offset%256;
     uint8_t value_mem = mem.ram[high][low];
-    cpu.a -= value_mem + cpu.y;
+    cpu.a =cpu.a- value_mem + cpu.x;
 
     uint8_t value_a = cpu.a;
 
@@ -44,9 +88,15 @@ void sbcabsx()
     else setsr(0);
     //modify N flag
     if(value_a<0)setsr(7);
+    else unsetsr(7);
     //modify z flag
     if(value_a==0)setsr(1);
+    else unsetsr(1);
     //modifi V flag
-    if(256-value_mem>cpu.y)setsr(6);
+    if(value_a > 127 || value_a < -127) {
+        setsr(6);
+    } else {
+        unsetsr(6);
+    }
 
 }
