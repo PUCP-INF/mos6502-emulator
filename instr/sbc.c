5bb6beadec97d9abc70c7459ce016bfe811674e9
Kotoamatsukam <alfredo.tarazona00@gmail.com>
2020-11-08 20:19:17 -0500

    sbcindy terminado, segunda prueba 2.1

--- instr/sbc.c
+++ instr/sbc.c
@@ -31,7 +31,32 @@ void sbcxind()
 }
 
 void sbcindy()
-{}
+{
+    uint8_t dm = get_arg(1);
+    uint8_t high=mem.ram[0][dm+1];
+    uint8_t low=mem.ram[0][dm];
+    uint16_t offset =(high<<8)+low+cpu.y;
+    cpu.a =cpu.a - mem.ram[offset>>8][offset & 0XFF];
+    uint8_t auxiliar = cpu.a;
+    uint8_t value_mem=mem.ram[offset>>8][offset & 0XFF];
+    //Vamos a modifiar las banderas
+    //modigy C carry
+    //RAA ESTO RAAA
+    if(auxiliar<0)unsetsr(0);
+    else setsr(0);
+    //modify N flag
+    if(auxiliar<0)setsr(7);
+    else unsetsr(7);
+    //modify z flag
+    if(auxiliar==0)setsr(1);
+    else unsetsr(1);
+    //modify V flag overflow
+    if(value_mem > 127 || value_mem < -127) {//se pasa de los rangos
+        setsr(6);
+    } else {
+        unsetsr(6);
+    }
+}
 
 void sbczpgx()
 {
